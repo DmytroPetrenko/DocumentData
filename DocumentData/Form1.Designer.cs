23c960d44781ed915c400b57329c02f89df56c60
Dmytro Petrenko <dmitry.a.petrenko@gmail.com>
2019-07-09 16:13:56 +0300

    TakeDocumentType()

--- DocumentData/Form1.Designer.cs
+++ DocumentData/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.StartBtn = new System.Windows.Forms.Button();
             this.YearComboBox = new System.Windows.Forms.ComboBox();
             this.OvuNumberComboBox = new System.Windows.Forms.ComboBox();
+            this.SelectFolderToSaveBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -44,17 +45,18 @@
             // 
             // StartBtn
             // 
-            this.StartBtn.Location = new System.Drawing.Point(464, 320);
+            this.StartBtn.Location = new System.Drawing.Point(396, 321);
             this.StartBtn.Name = "StartBtn";
             this.StartBtn.Size = new System.Drawing.Size(119, 23);
             this.StartBtn.TabIndex = 1;
             this.StartBtn.Text = "Start";
             this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
             // 
             // YearComboBox
             // 
             this.YearComboBox.FormattingEnabled = true;
-            this.YearComboBox.Location = new System.Drawing.Point(337, 320);
+            this.YearComboBox.Location = new System.Drawing.Point(142, 323);
             this.YearComboBox.Name = "YearComboBox";
             this.YearComboBox.Size = new System.Drawing.Size(121, 21);
             this.YearComboBox.TabIndex = 2;
@@ -62,16 +64,27 @@
             // OvuNumberComboBox
             // 
             this.OvuNumberComboBox.FormattingEnabled = true;
-            this.OvuNumberComboBox.Location = new System.Drawing.Point(210, 320);
+            this.OvuNumberComboBox.Location = new System.Drawing.Point(269, 323);
             this.OvuNumberComboBox.Name = "OvuNumberComboBox";
             this.OvuNumberComboBox.Size = new System.Drawing.Size(121, 21);
             this.OvuNumberComboBox.TabIndex = 3;
+            // 
+            // SelectFolderToSaveBtn
+            // 
+            this.SelectFolderToSaveBtn.Location = new System.Drawing.Point(522, 321);
+            this.SelectFolderToSaveBtn.Name = "SelectFolderToSaveBtn";
+            this.SelectFolderToSaveBtn.Size = new System.Drawing.Size(134, 23);
+            this.SelectFolderToSaveBtn.TabIndex = 4;
+            this.SelectFolderToSaveBtn.Text = "Select folder to save";
+            this.SelectFolderToSaveBtn.UseVisualStyleBackColor = true;
+            this.SelectFolderToSaveBtn.Click += new System.EventHandler(this.SelectFolderToSaveBtn_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SelectFolderToSaveBtn);
             this.Controls.Add(this.OvuNumberComboBox);
             this.Controls.Add(this.YearComboBox);
             this.Controls.Add(this.StartBtn);
@@ -89,6 +102,7 @@
         private System.Windows.Forms.Button StartBtn;
         private System.Windows.Forms.ComboBox YearComboBox;
         private System.Windows.Forms.ComboBox OvuNumberComboBox;
+        private System.Windows.Forms.Button SelectFolderToSaveBtn;
     }
 }
 
--- DocumentData/Form1.cs
+++ DocumentData/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,115 @@ namespace DocumentData
 {
     public partial class Form1 : Form
     {
+        private string finalFilderPath = @"C:\Dmytro";
+        private string FinalFolderPath { get { return finalFilderPath; } set { finalFilderPath = value; } }
         public Form1()
         {
             InitializeComponent();
+
+            using (ovuEntities db = new ovuEntities())
+            {
+                // Заполнение списка с годами
+                var proceedings = db.proceedings;
+                List<int> years = new List<int>();
+                foreach (var item in proceedings)
+                {
+                    if (!years.Contains(item.created_at.Year))
+                    {
+                        years.Add(item.created_at.Year);
+                    }
+                }
+                YearComboBox.DataSource = years;
+                OvuNumberComboBox_SelectedIndexChanged(new object(), new EventArgs());
+                YearComboBox.SelectedIndexChanged += OvuNumberComboBox_SelectedIndexChanged;
+            }
+        }
+
+        void OvuNumberComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            using (ovuEntities db = new ovuEntities())
+            {
+                var proceedings = db.proceedings;
+                List<string> ovuNumbers = new List<string>();                
+                foreach (var item in proceedings)
+                {
+                    if ((int)YearComboBox.SelectedItem == item.created_at.Year)
+                    {
+                        ovuNumbers.Add(item.number);
+                    }
+                }
+                OvuNumberComboBox.DataSource = ovuNumbers;
+            }
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            TakeDocumentType();
+        }
+
+        private void TakeDocumentType()
+        {
+            using (var db = new ovuEntities())
+            {
+                var article_types = db.article_types;
+                List<string> documentTypeArr = new List<string>();
+                List<int?> articles_id = new List<int?>();
+                List<int?> article_type_id = new List<int?>();
+                int proceedings_id = 0;
+                foreach (var item in db.proceedings)
+                {
+                    if (item.created_at.Year == (int)YearComboBox.SelectedItem && item.number == (string)OvuNumberComboBox.SelectedItem)
+                    {
+                        proceedings_id = item.id;                        
+                    }
+                }
+
+                foreach (var item2 in db.articles)
+                {
+                    if (item2.proceedings_id == proceedings_id)
+                    {
+                        articles_id.Add(item2.id);                     
+                    }
+                }
+                int? check = 0;
+                foreach (var item3 in articles_id)
+                {
+                    foreach (var item4 in db.collections)
+                    {
+                        
+                        if (item4.articles_id == item3 && item4.articles_id != check)
+                        {
+                            article_type_id.Add(item4.article_type_id);
+                            check = item4.articles_id;
+                        }
+                    }
+                }
+
+                foreach (var item5 in article_type_id)
+                {
+                    foreach (var item6 in db.article_types)
+                    {
+                        if (item5 == item6.id)
+                        {
+                            documentTypeArr.Add(item6.title);
+                            textBox1.Text += item6.title;
+                        }
+                    }
+                }
+
+                File.WriteAllLines(FinalFolderPath + "\\" + "DocumentsType_" + ((int)YearComboBox.SelectedItem).ToString() + "_" + (string)OvuNumberComboBox.SelectedItem + ".txt", documentTypeArr);
+            }
+        }
+
+        private void SelectFolderToSaveBtn_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                dialog.RootFolder = Environment.SpecialFolder.MyComputer;
+                dialog.ShowDialog();
+                textBox1.Text += "Выбрана папка: " + dialog.SelectedPath;
+                FinalFolderPath = dialog.SelectedPath;
+            }
         }
     }
 }
